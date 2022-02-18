1ad715b0adc4a023e8d0deda36029ef00450911b
Jia Song <jiasong214@gmail.com>
2022-02-18 22:32:27 +1100

    create signin/signup pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from './routes/Signin';
+import OwnerHome from './routes/PetOwner/Home';
+
+import './style/global.scss';
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+
+          <Route path="/">
+            <Route index element={<Signin />} />
+
+            <Route path="owner" element={<OwnerHome />} >
+              {/* pet owner's page here */}
+              <Route path="" />
+            </Route>
+
+            <Route path="walker" >
+              {/* pet walker's page here */}
+              
+            </Route>
+          </Route>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
