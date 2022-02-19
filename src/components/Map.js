dbf312e6e7d3b7d1fa26c9b7ff28bc7944b0434c
Jia Song <jiasong214@gmail.com>
2022-02-19 16:58:36 +1100

    connect to user info api

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Map = () => {
+  const [data, setData] = useState();
+  
+  useEffect(() => {
+    // data fetching here
+  },[]);
+
   return (
     <div className="map">
       map here
