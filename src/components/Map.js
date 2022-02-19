b0e69e24dd75e0f3697e84843505fb6730d072a7
Laurence Qi <laurence.qi@outlook.com>
2022-02-19 16:51:58 +1100

    added map direction Iframe

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Map = () => {
+const Map = () => { //TODO: Plug in the actual to and from directions for the walker when they accept a job.
   return (
     <div className="map">
-      map here
+      <iframe
+          width="450"
+          height="250"
+          frameBorder="0"
+          style={{border:0}}
+          loading="lazy"
+          src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyAm7vYw4jkC7m9hbEKpMfFxjwLAOZgxwko&origin=-33.862477,150.951833&destination=-33.8724235,151.2591179&avoid=tolls|highways"
+          allowFullScreen>
+      </iframe>
     </div>
   )
 }
