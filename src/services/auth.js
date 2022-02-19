5da4713a08a4f5f4bcf4420377c2070735024557
Jia Song <jiasong214@gmail.com>
2022-02-19 21:45:33 +1100

    implement context to manage user globally

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,2 +1,53 @@
-// all the auth API's here
-// so in components we can just import them and use it.
+import { get, post } from "../network/http";
+import { clearToken, getToken, setToken } from "../localStorage/token";
+
+const signIn = async (email, password) => {
+  console.log(email, password)
+  const res = await post(`/user_token`, {
+    auth: {
+      email, password
+    }
+  });
+  const token = await res.data.jwt;
+
+  // set token in localStorage
+  setToken(token);
+
+  return token;
+}
+
+const checkSignIn = async () => {
+  // get token from localStorage
+  let token = getToken();
+
+  if(!token) return;
+
+  // TODO: api endpoint for this
+  let res = await get(`what is this?`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+
+  return res;
+}
+
+const signUp  = async ({userInfo}) => {
+  let res = await post(`/users.json`, {
+    ...userInfo
+  });
+
+  return res.data;
+}
+
+const logout = async () => {
+  // clear the localStorage
+  clearToken();
+}
+
+export {
+  signIn,
+  checkSignIn,
+  signUp,
+  logout,
+};
