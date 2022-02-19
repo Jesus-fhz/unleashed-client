3503551ed380f6696b0f860fdf10a5114aed4e9e
Jia Song <jiasong214@gmail.com>
2022-02-19 17:24:02 +1100

    fix api post error

--- src/services/users.js
+++ src/services/users.js
@@ -1,5 +1,16 @@
-import { get } from "../network/http";
+import { get, post } from "../network/http";
 
+const signIn = async (username, password) => {
+  let res = await get(``)
+}
+
+const signUp  = async ({userInfo}) => {
+  let res = await post(`/users.json`, {
+    ...userInfo
+  });
+
+  return res.data;
+}
 
 const fetchUserInfo = async (user_id) => {
   let res = await get(`/users/${user_id}.json`);
