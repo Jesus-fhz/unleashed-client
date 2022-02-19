761ed5d3c541707044fcd0e4d8cc544eb13ac46b
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-19 12:03:12 +1100

    Mergin changes, adding globals styes, ande deleted unnecesary changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Home  = () => {
   return (
-    <div>
-      This is a owner's home
+    <div class="navbar">
+      <div class="container flex">
+          <h1 class="logo">Unleashed</h1>
+          <nav>
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Pets</a></li>
+                  <li><a href="#">User</a></li>
+              </ul>
+          </nav>
+      </div>
     </div>
   )
 }
