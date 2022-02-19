d0ddd342b969dfc5d6f7b0b96cafa7702469baf2
Jesus Flores <jesus@Jesuss-MBP.modem>
2022-02-19 12:03:12 +1100

    Mergin changes, adding globals styes, ande deleted unnecesary changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Home from './routes/Home';
 import './style/global.scss';
 
 function App() {
   return (
     <div className="App">
-      you are in app
+       <Home/>
     </div>
   );
 }
