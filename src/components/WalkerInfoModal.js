da8f0c5f077dcad0e292514a7ec2497737341b80
Jia Song <jiasong214@gmail.com>
2022-02-22 00:46:26 +1100

    improve style

---
+++ src/components/WalkerInfoModal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const WalkerInfoModal = ({
+  data
+}) => {
+  return (
+    <div className="walkerInfoModal">
+      <dialog>
+        <img src="" alt="" />
+        <h2>Name here</h2>
+
+        <div className="btn-container">
+          <button onClick={}>
+            Accept
+          </button>
+          <button onClick={}>
+            Decline
+          </button>
+        </div>
+      </dialog>
+    </div>
+  )
+}
+
+export default WalkerInfoModal;
