134d246970caa8a49cb764046bce24e4e08307f3
Jia Song <jiasong214@gmail.com>
2022-02-21 13:34:32 +1100

    style organiseWalkModal

--- src/components/UserPetList.js
+++ src/components/UserPetList.js
@@ -2,12 +2,10 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import { fetchUserPets } from '../services/pets';
-import edit_icon from '../assets/images/edit_icon.png';
-import dog_icon from '../assets/images/dog_icon.svg';
-import user_icon from '../assets/images/user_icon.svg';
-import home_icon from '../assets/images/home_icon.svg';
-import '../style/userPetList.scss'
 import Nav from './Nav';
+import OrganiseWalkModal from './OrganiseWalkModal';
+import edit_icon from '../assets/images/edit_icon.png';
+import '../style/userPetList.scss'
 
 
 const UserPetList = () => {
@@ -15,6 +13,7 @@ const UserPetList = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [selectedPet, setSelectedPet] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const authContext = useContext(AuthContext);
 
   // fetch user's all pets when the component is rendered
@@ -26,19 +25,22 @@ const UserPetList = () => {
       .finally(() => setLoading(false));
   }, []);
 
-  const selectPet = (id) => {
-    const isExist = selectedPet.some((item) => item === id);
+  const selectPet = (pet) => {
+    const isExist = selectedPet.some((item) => item.id === pet.id);
 
     if(isExist) {
-      setSelectedPet(selectedPet.filter(item => item !== id));
+      setSelectedPet(selectedPet.filter(item => item.id !== pet.id));
     } else {
-      setSelectedPet([...selectedPet, id]);
+      setSelectedPet([...selectedPet, pet]);
     }
   }
 
-  const organiseWalk = () => {
-    console.log(selectedPet)
+  const handleModal = () => {
+    isModalOpen ? setIsModalOpen(false) : setIsModalOpen(true);
+  }
 
+  const organiseWalk = () => {
+    handleModal();
   }
 
 
@@ -53,53 +55,62 @@ const UserPetList = () => {
         ?
         <p>error...</p>
         :
-        <div className="userPetList">
-          <h1>
-            <Link to="/">Unleashed</Link>
-          </h1>
-          <div className="scroll-container">
-            <ul>
-              {pets.map((pet) => (
-                <li key={pet.id}>
-                  <div className="item-innerbox">
-                    <Link to="/" className="editBtn">
-                      <img src={edit_icon} alt="edit" />
-                    </Link>
-                    <div className="img-container">
-                      <img src={pet.image} alt={pet.name} />
+        <>
+          <OrganiseWalkModal
+            isOpen={isModalOpen}
+            handleModal={handleModal}
+            selectedPet={selectedPet} 
+          />
+          <div className="userPetList">
+            <h1>
+              <Link to="/">Unleashed</Link>
+            </h1>
+            <div className="scroll-container">
+              <ul>
+                {pets.map((pet) => (
+                  <li key={pet.id}>
+                    <div className="item-innerbox">
+                      <Link to="/" className="editBtn">
+                        <img src={edit_icon} alt="edit" />
+                      </Link>
+                      <div className="img-container">
+                        <img src={pet.image} alt={pet.name} />
+                      </div>
+                      <div className="text-container">
+                        <h3>{pet.name}</h3>
+                        <p className="breed">{pet.breed}, {pet.age} Years old</p>
+                      </div>
                     </div>
-                    <div className="text-container">
-                      <h3>{pet.name}</h3>
-                      <p className="breed">{pet.breed}, {pet.age} Years old</p>
-                    </div>
-                  </div>
-                  <button 
-                    className="selectBtn"
-                    onClick={() => selectPet(pet.id)}
-                  >
-                    {
-                    selectedPet.some((id) => id === pet.id) 
-                    ?
-                    "Cancel"
-                    :
-                    "Select"
-}
-                  </button>
-                </li>
-              ))}
-            </ul>
-            {/* <Link to="/owner/register" className="addBtn">
-              +
-            </Link> */}
-            {
-            selectedPet.length > 0 
-            ? 
-            <button onClick={() => organiseWalk()}>Organise walk</button> 
-            : ""
-            }
+                    <button 
+                      className="selectBtn"
+                      onClick={() => selectPet(pet)}
+                    >
+                      {
+                      selectedPet.some((item) => item.id === pet.id) 
+                      ?
+                      "Cancel"
+                      :
+                      "Select"
+  }
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              {
+              selectedPet.length > 0 
+              ? 
+              <button 
+                className="organiseBtn"
+                onClick={() => organiseWalk()}
+              >
+                {`Organise walk for ${selectedPet.length} pets`}
+              </button> 
+              : ""
+              }
+            </div>
+            <Nav />
           </div>
-          <Nav />
-        </div>
+        </>
       }
     </>
   )
