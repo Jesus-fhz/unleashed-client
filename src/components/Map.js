da77c290cf62cc0c08a38d0e093cd630a1f9ee7e
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-02-25 10:56:38 +1100

    trying to bug fix

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,7 +7,6 @@ import {useInterval} from './UseInterval'
 import { Link } from 'react-router-dom';
 import { getLocation } from '../services/walk';
 
-
 let intervalID1;
 let intervalID2;
 
@@ -63,11 +62,8 @@ function Map({isFinding, showRadar}) {
           // setWalkerPosition(auth.location)
         }, 1000);
       }
-      if(auth.status === "ongoing"){
-        
-        // add polling here aswell. 
-      }
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth.status])
   
   //WALKER USE EFFECT
@@ -92,6 +88,7 @@ function Map({isFinding, showRadar}) {
     }
 
     // return () => clearInterval(intervalID);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth, currentPosition]);
 
   useEffect(()=> {
@@ -147,13 +144,17 @@ function Map({isFinding, showRadar}) {
     const yCorrect = !(yOver || yUnder);
 
     if( xCorrect && yCorrect ){
+      // console.log('both x and y correct')
       //setState for the walk done. 
       setMoverLocation(stationaryLocation);
       auth.changeStatus("pickup");
+      console.log(auth.status)
+      // fakeWalk(moverLocation, setMoverLocation, stationaryLocation)
       return;
     }
     else {
-      // if current position within range of destination then don't perform fake move 
+      // if current position within range of destination then don't perform fake move
+
       if (xUnder) { 
         x = incrementDistance;
       } else if (xOver){
@@ -174,6 +175,9 @@ function Map({isFinding, showRadar}) {
   }
 
   const fakeWalk = (moverLocation, setMoverLocation, stationaryLocation) => {
+
+    console.log('hellooooo fakewalk started')
+
     let x = 0.00004 * Math.cos(angle);
     let y = 0.00004 * Math.sin(angle);
     
